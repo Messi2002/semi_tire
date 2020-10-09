ae03f040801216ddff021592eb6fb97192e0085a
Messi2002 <goodyutaka@hotmail.com>
2020-10-09 18:27:34 +0900

    マイページ機能の実装
    user_idカラムの追加
    アソシエーションをモデルに定義
    マイページボタンの作成
    マイページ画面の作成
    投稿者の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_072158) do
+ActiveRecord::Schema.define(version: 2020_10_09_081014) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
